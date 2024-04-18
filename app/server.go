761600762d88b6a17be4faf0b4a334d6c327c4cb
David Frank <frankd@in.tum.de>
2024-04-18 12:44:34 +0200

    try sending multiple PONGs

--- app/server.go
+++ app/server.go
@@ -6,27 +6,34 @@ import (
 	"os"
 )
 
+func handleClient(conn net.Conn) {
+	defer conn.Close()
+
+	b := []byte("+PONG\r\n")
+	_, err = conn.Write(b)
+
+	if err != nil {
+		fmt.Println("Error writing to connection: ", err.Error())
+		return
+	}
+}
+
 func main() {
 	l, err := net.Listen("tcp", "0.0.0.0:6379")
 	if err != nil {
 		fmt.Println("Failed to bind to port 6379")
 		os.Exit(1)
 	}
+	defer l.Close()
 
 	for {
 		conn, err := l.Accept()
-		defer conn.Close()
 		if err != nil {
 			fmt.Println("Error accepting connection: ", err.Error())
 			os.Exit(1)
 		}
 
-		b := []byte("+PONG\r\n")
-		_, err = conn.Write(b)
+		handleClient(conn)
 
-		if err != nil {
-			fmt.Println("Error writing to connection: ", err.Error())
-			os.Exit(1)
-		}
 	}
 }
