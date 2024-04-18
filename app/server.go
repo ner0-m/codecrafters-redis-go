82bac196f1565509d574ea54a525c58981933177
David Frank <frankd@in.tum.de>
2024-04-18 12:38:12 +0200

    try sending PONG

--- app/server.go
+++ app/server.go
@@ -12,9 +12,14 @@ func main() {
 		fmt.Println("Failed to bind to port 6379")
 		os.Exit(1)
 	}
-	_, err = l.Accept()
+
+    conn, err := l.Accept()
+    defer conn.Close()
 	if err != nil {
 		fmt.Println("Error accepting connection: ", err.Error())
 		os.Exit(1)
 	}
+
+    b := []byte("+PONG\r\n")
+    conn.Write(b)
 }
